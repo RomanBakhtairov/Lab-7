2eb16bcf0c3a47a5165d063999bc4ae2113072aa
RomanBakhtairov <romanbakhtairov@gmail.com>
2024-04-06 13:28:43 +0300

    Final commit

--- main.py
+++ main.py
@@ -3,8 +3,9 @@ import matplotlib.pyplot as plt
 import time as tm
 from mpl_toolkits.mplot3d import Axes3D
 import random as rd
-
+#Каждый метод есть в функции start на строке 77
 #Я взял вариант № 2
+##Задание №1, проверка работы numpy
 def numPyTest():
     nparray = np.random.randint(1,100,size=2000000)
     pyarray = [rd.randint(1,100) for i in range(2000000)]
@@ -24,8 +25,7 @@ def numPyTest():
     print(f"Время умножения в NumPy: {timeN}")
     print(f'Время умножения в python:{timeP}')
     print(f'Numpy быстрее стандарта в {round(timeP/timeN)} раза')
-
-
+#Задание №2, построение гистограммы 
 def myHist():
     arr = np.genfromtxt('data2.csv', delimiter=',')
     arr = arr[1:]
@@ -38,56 +38,57 @@ def myHist():
     ax.hist(pH, 16)
     ax.grid()
     plt.show()
+#Задание №3, построение функции в 3D
+def drow3D_function():
+      x_value = np.linspace(-2*np.pi,2*np.pi, 100)
+      y_value = np.sin(x_value)*np.cos(x_value)
+      z_value = np.sin(x_value)*np.cos(x_value)
+      frame = plt.figure()
+      ax = frame.add_subplot(111, projection='3d')
+      ax.plot(x_value, y_value, z_value, marker='x', c='red')
+      plt.show()          
+#Доп задание
+def move_graph_version1():
+    x_value = [0]
+    y_value = [0]
+    for i in range(50):
+        x_value.append(x_value[-1]+0.2)
+        y_value.append(np.sin(x_value[-1]))
+        plt.xlim(0,10)
+        plt.ylim(-2, 2)
+        plt.scatter(x_value, y_value,color = 'black')
+        plt.pause(0.0001)
+    plt.show()
+def move_graph_version2():
+    x_value = np.linspace(-10,10,40)
+    edit_x = x_value
+    y_value = np.sin(x_value)
+    for i in range(100):
+        plt.clf()
+        edit_x = edit_x +0.2 
+        y_value= np.sin(edit_x)
+        plt.xlim(0,10)
+        plt.ylim(-2, 2)
+        plt.scatter(x_value, y_value,color = 'black')
+        plt.pause(0.0001)
+    plt.show()
 
-myHist()
+def Start():
+#Задание 1
 
-# def start():
-#     arr = np.array([5, 9, 10, 'ad', 7])
-#     arr = arr[::-1]
-#     print(arr)
+    #numPyTest()
 
+#Задание 2
 
-# def tableLoad():
-#     arr = np.genfromtxt('voltage.csv', delimiter=',')
-#     time = arr[:100,0]
-#     time = time[:,np.newaxis]
-#     curr = arr[:100,1]
-#     curr = curr[:,np.newaxis]
-#     volt = arr[:100,2]
-#     volt = volt[:,np.newaxis]
+    #myHist()
 
-#     plt.plot(time, curr * 50, 'b', time, volt, 'r')
-#     plt.show()
+#Задание 3
 
+    #drow3D_function()
 
-# def hist():
-#     arr = np.genfromtxt('test.csv', delimiter=',')
-#     arr = arr[1:]
-#     daysInYear = 365.25
+#Доп задание(в 2 вариантах)
 
-#     age = np.int_(arr[:,1] / daysInYear)
-
-#     fig = plt.figure(figsize=(6, 4))
-#     ax = fig.add_subplot()
-#     ax.hist(age, 100, (50, 60))
-#     ax.grid()
-#     plt.show()
-
-
-# def plot3d():
-#     np.random.seed(40)
-#     xs = np.linspace(0, 10, 20)
-#     ys = xs
-#     zs = np.sin(xs)
-
-#     fig = plt.figure()
-#     ax = fig.add_subplot(111, projection='3d')
-#     ax.plot(xs, ys, zs, marker='x', c='red')
-#     plt.show()
-
-
-# if __name__ == '__main__':
-#     #start()
-#     tableLoad()
-#     #hist()
-#     #plot3d()
+    #move_graph_version1()
+    #move_graph_version2()
+    pass
+Start()
